d88fe77d74c14ca3a8f6723cc2f95747d49a5fd6
ivankp <ivan.pogrebnyak@gmail.com>
2015-02-15 22:37:39 -0600

    my same as fj with kt algorithm

--- fj.cc
+++ fj.cc
@@ -6,15 +6,20 @@
 
 using namespace std;
 
+template<typename T> inline T sq(T x) { return x*x; }
+
 istream& operator>>(istream &in, vector<fastjet::PseudoJet>& p) {
   static double px, py, pz, E;
   in >> px >> py >> pz >> E;
-  p.push_back(fastjet::PseudoJet(px,py,pz,E));
+  if (in) p.push_back(fastjet::PseudoJet(px,py,pz,E));
   return in;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+  double R = 0.6;
+  if (argc>1) R = atof(argv[1]);
+
   fastjet::ClusterSequence::print_banner();
   cout << endl;
 
@@ -25,11 +30,12 @@ int main()
   while (cin >> particles) {
     fastjet::PseudoJet &p = particles.back();
     cout << setw(12) << p.pt() << ", "
-         << setw(12) << p.kt_distance(beam) << endl;
+         << setw(12) << p.rap() << ", "
+         << setw(12) << sq(p.pt()*p.eta()) << endl;
   }
   cout << endl;
 
-  fastjet::JetDefinition jet_def(fastjet::antikt_algorithm, 0.6);
+  fastjet::JetDefinition jet_def(fastjet::kt_algorithm, R);
 
   fastjet::ClusterSequence seq(particles, jet_def, true);
 
--- my.cc
+++ my.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <list>
+#include <set>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
 template<typename T> inline T sq(T x) { return x*x; }
+
+template<class ForwardIt> ForwardIt next(ForwardIt it) { return ++it; }
 
 class particle {
 private:
@@ -24,13 +27,18 @@ public:
     _phi(px == 0. && px == 0. ? 0. : atan2(py,px)),
     _M(sqrt(_E*_E-_P*_P)), _d(sq(_pt*_eta)), _mothers(NULL,NULL)
   { }
+  ~particle() {
+    // delete _mothers.first;
+    // delete _mothers.second;
+  }
 
   particle operator+(const particle &p) const {
     particle pp(_px+p._px,_py+p._py,_pz+p._pz,_E+p._E);
-    pp._mothers = make_pair(this,&p);
+    pp._mothers = make_pair(new particle(*this),new particle(p));
     return pp;
   }
 
+  int     id() const { return  _id; }
   double  px() const { return  _px; }
   double  py() const { return  _py; }
   double  pz() const { return  _pz; }
@@ -44,21 +52,30 @@ public:
     return _mothers;
   }
 
+  double R(const particle &p) const {
+    return sqrt( sq(_eta-p._eta)+sq(_phi-p._phi) );
+  }
+
   double d() const { return _d; }
   double d(const particle &p) const {
     return sq( min(_pt,p._pt) ) * ( sq(_eta-p._eta)+sq(_phi-p._phi) );
   }
 
+  bool operator<(const particle &p) const {
+    if (_d==p._d) return ( _id < p._id );
+    else return ( _d < p._d );
+  }
+
   ostream& prt(ostream &out, int depth=0) const {
-    for (int i=0;i<depth;++i) out << "  ";
+    for (int i=0;i<depth;++i) out << "  |";
     out << fixed
-        << setw( 3) << _id << ": ("
-        // << setw(12) << _px << ", "
-        // << setw(12) << _py << ", "
-        // << setw(12) << _pz << ", "
-        // << setw(12) <<  _E
-        << setw(12) << _pt << ", "
-        << setw(12) << _d
+        << setw(3) << _id << ": ("
+        << setw(9) << _px << ", "
+        << setw(9) << _py << ", "
+        << setw(9) << _pz << ", "
+        << setw(9) <<  _E << " | "
+        << setw(9) << _pt << ", "
+        << setw(9) << _d
         << " )" << endl;
     if (_mothers.first)  _mothers.first ->prt(out,depth+1);
     if (_mothers.second) _mothers.second->prt(out,depth+1);
@@ -69,31 +86,93 @@ public:
 int particle::num = 0;
 
 ostream& operator<<(ostream &out, const particle& p) {
-  const ios_base::fmtflags flags = out.flags(
-    ios::right | ios::showpoint | ios::fixed | ios::scientific
-  );
-  const streamsize prec = out.precision(8);
+  const ios_base::fmtflags flags = out.flags();
+  const streamsize prec = out.precision(5);
   p.prt(out);
   out.flags(flags);
   out.precision(prec);
   return out;
 }
 
-istream& operator>>(istream &in, list<particle>& p) {
+typedef set<particle>::iterator iter_t;
+
+istream& operator>>(istream &in, set<particle>& p) {
   static double px, py, pz, E;
   in >> px >> py >> pz >> E;
-  p.push_back(particle(px,py,pz,E));
+  if (in) cout << *p.insert(particle(px,py,pz,E)).first;
   return in;
 }
 
-typedef list<particle>::iterator iter_t;
+void cluster(set<particle>& particles, list<particle>& jets, double R) {
+  iter_t it = particles.begin();
+  iter_t end = particles.end(), it1 = end, it2 = end;
+  bool merged = false;
+  double dist = it->d();
+  double merged_R = 0.;
+  for (;it!=end;++it) {
+    for (iter_t jt=next(it);jt!=end;++jt) {
+      double _R = it->R(*jt);
+      if ( _R <= R ) { // only if particles are close enough
+        double d = it->d(*jt);
+        if (d < dist) {
+          dist = d;
+          merged_R = _R;
+          it1 = it;
+          it2 = jt;
+          if (!merged) merged = true;
+        }
+      }
+    }
+  }
+  if (merged) {
+    particles.insert((*it1)+(*it2));
+    particles.erase(it1);
+    particles.erase(it2);
+    cout << "Merged "
+         << setw(2) << it1->id() << " & "
+         << setw(2) << it2->id() << "  d = ";
+    const ios_base::fmtflags flags = cout.flags();
+    cout << fixed << setprecision(8) << dist
+         << " R = " << merged_R << endl;
+    cout.flags(flags);
+  } else {
+    jets.push_back(*particles.begin());
+    particles.erase(particles.begin());
+    cout << jets.back().id() << " is a jet" << endl;
+  }
+}
 
-int main()
+int main(int argc, char **argv)
 {
-  list<particle> particles;
+  double R = 0.6;
+  if (argc>1) R = atof(argv[1]);
+
+  set<particle> particles;
   cout << "Original particles:" << endl;
-  while (cin >> particles) cout << particles.back();
+  cout << setw(10) << "px"
+       << setw(11) << "py"
+       << setw(11) << "pz"
+       << setw(10) << "E"
+       << setw(13) << "pt"
+       << setw(10) << "d"
+       << endl;
+  while (cin >> particles);
   cout << endl;
+
+  cout << "Sorted by d:" << endl;
+  for (iter_t it=particles.begin(),end=particles.end();it!=end;++it) {
+    cout << *it;
+  }
+  cout << endl;
+
+  cout << "Max R = " << R << endl << endl;
+
+  list<particle> jets;
+  while (particles.size()) cluster(particles, jets, R);
+  cout << endl;
+
+  for (list<particle>::iterator it=jets.begin(),
+       end=jets.end();it!=end;++it) cout << *it;
 
   return 0;
 }
